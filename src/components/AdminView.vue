e1da08490d3b8b3961874d58e84076f97c0aaa48
Iago Maciel <iagommendes@gmail.com>
2025-03-13 05:16:53 -0300

    Adiciona funcionalidade de login e gerenciamento de política de privacidade com modal. Inclui componentes de Modal e AdminView, além de um banco de dados simulado.

---
+++ src/components/AdminView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <h2>Usuários que aceitaram a política</h2>
+      <ul>
+        <li v-for="user in acceptedUsers" :key="user.id">{{ user.name }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        users: []
+      };
+    },
+    computed: {
+      acceptedUsers() {
+        return this.users.filter(user => user.acceptedPolicy);
+      }
+    },
+    created() {
+      fetch('http://localhost:3000/users')
+        .then(response => response.json())
+        .then(data => {
+          this.users = data;
+        });
+    }
+  }
+  </script>
